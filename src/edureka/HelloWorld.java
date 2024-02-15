4a8830ae40e635dda40db899b2dc36cdd0544304
Apeksha Duraphe <Apeksha Duraphe@DESKTOP-J9T21OU>
2024-02-15 23:15:14 +0530

    commit

---
+++ src/edureka/HelloWorld.java
@@ -0,0 +1,10 @@
+package edureka;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+System.out.println("Hello world");
+}
+}
+
+
+
---
+++ src/edureka/UnaryOperator.java
@@ -0,0 +1,12 @@
+package edureka;
+
+public class UnaryOperator {
+public static void main(String[] args) {
+	int x=10;
+	System.out.println(x++);
+	System.out.println(++x);
+	System.out.println(x--);
+	System.out.println(--x);
+}
+
+}
